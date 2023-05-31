fd42de0f48e264434ec218372e13125b1f220320
Dan Tarff <daniel.tarff@dazn.com>
2023-05-31 16:18:10 +0100

    Create VPC stack

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = "vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
--- terraform/vpc/provider.tf
+++ terraform/vpc/provider.tf
@@ -1,6 +1,7 @@
 provider "aws" {
-  alias  = "euc1"
   region = "eu-central-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                 = "default"
 
   default_tags {
     tags = local.default_tags
