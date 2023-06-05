147f0a5b9ae50a40eb07461a87e98dc50b8486df
Dan Tarff <daniel.tarff@dazn.com>
2023-06-05 19:54:49 +0100

    Simplified ECR

--- terraform/ecr/main.tf
+++ terraform/ecr/main.tf
@@ -1,24 +1,3 @@
-module "ecr" {
-  source = "terraform-aws-modules/ecr/aws"
-  version = "1.6.0"
-
-  repository_name = "private-repo"
-
-  repository_lifecycle_policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1,
-        description  = "Keep last 30 images",
-        selection = {
-          tagStatus     = "tagged",
-          tagPrefixList = ["v"],
-          countType     = "imageCountMoreThan",
-          countNumber   = 30
-        },
-        action = {
-          type = "expire"
-        }
-      }
-    ]
-  })
+resource "aws_ecr_repository" "ecr_repo" {
+  name = "ecr-repo"
 }
