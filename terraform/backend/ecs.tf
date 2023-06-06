a929adfe3404a4d51f1d05c4206c942316da5f6d
Dan Tarff <daniel.tarff@dazn.com>
2023-06-06 22:01:34 +0100

    Create an ECS cluster and service, update ALB and SGs

--- terraform/backend/ecs.tf
+++ terraform/backend/ecs.tf
@@ -1,106 +1,96 @@
-module "ecs" {
-  source  = "terraform-aws-modules/ecs/aws"
-  version = "5.0.1"
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "${local.app_name}-cluster"
+}
 
-  cluster_name = "ecs-integrated"
-
-  cluster_configuration = {
-    execute_command_configuration = {
-      logging = "OVERRIDE"
-      log_configuration = {
-        cloud_watch_log_group_name = "/aws/ecs/aws-ec2"
+resource "aws_ecs_task_definition" "devops_app" {
+  family                   = local.app_name
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "${local.app_name}",
+      "image": "${aws_ecr_repository.ecr_repo.repository_url}",
+      "essential": true,
+      "environment": [
+        {
+          "name": "POSTGRESQL_HOST",
+          "value": "${module.rds.db_instance_address}"
+        },
+        {
+          "name": "POSTGRESQL_DBNAME",
+          "value": "${module.rds.db_instance_name}"
+        },
+        {
+          "name": "POSTGRESQL_USERNAME",
+          "value": "${module.rds.db_instance_username}"
+        },
+        {
+          "name": "POSTGRESQL_PASSWORD",
+          "value": "${module.rds.db_instance_password}"
+        }
+      ],
+      "portMappings": [
+        {
+          "containerPort": 8080,
+          "hostPort": 8080
+        }
+      ],
+      "memory": 512,
+      "cpu": 256,
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+          "awslogs-group": "${local.app_name}",
+          "awslogs-region": "eu-central-1",
+          "awslogs-create-group": "true",
+          "awslogs-stream-prefix": "${local.app_name}"
+        }
       }
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  cpu                      = 256
+  execution_role_arn       = aws_iam_role.task_execution_role.arn
+}
+
+resource "aws_iam_role" "task_execution_role" {
+  name               = "taskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
     }
   }
+}
 
-  fargate_capacity_providers = {
-    FARGATE_SPOT = {
-      default_capacity_provider_strategy = {
-        weight = 100
-      }
-    }
+resource "aws_iam_role_policy_attachment" "task_execution_role_policy" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "devops_app_service" {
+  name            = local.app_name
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.devops_app.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.target_group.arn
+    container_name   = aws_ecs_task_definition.devops_app.family
+    container_port   = 8080
   }
 
-  services = {
-    ecsdemo-frontend = {
-      cpu    = 1024
-      memory = 4096
-
-      # Container definition(s)
-      container_definitions = {
-
-        fluent-bit = {
-          cpu       = 512
-          memory    = 1024
-          essential = true
-          image     = "906394416424.dkr.ecr.us-west-2.amazonaws.com/aws-for-fluent-bit:stable"
-          firelens_configuration = {
-            type = "fluentbit"
-          }
-          memory_reservation = 50
-        }
-
-        ecs-sample = {
-          cpu       = 512
-          memory    = 1024
-          essential = true
-          image     = "public.ecr.aws/aws-containers/ecsdemo-frontend:776fd50"
-          port_mappings = [
-            {
-              name          = "ecs-sample"
-              containerPort = 80
-              protocol      = "tcp"
-            }
-          ]
-
-          # Example image used requires access to write to root filesystem
-          readonly_root_filesystem = false
-
-          dependencies = [{
-            containerName = "fluent-bit"
-            condition     = "START"
-          }]
-
-          enable_cloudwatch_logging = false
-          log_configuration = {
-            logDriver = "awsfirelens"
-            options = {
-              Name                    = "firehose"
-              region                  = "eu-west-1"
-              delivery_stream         = "my-stream"
-              log-driver-buffer-limit = "2097152"
-            }
-          }
-          memory_reservation = 100
-        }
-      }
-
-      load_balancer = {
-        service = {
-          target_group_arn = aws_alb_target_group.target_group.arn
-          container_name   = "ecs-sample"
-          container_port   = 80
-        }
-      }
-
-      subnet_ids = local.private_subnet_ids
-      security_group_rules = {
-        alb_ingress_3000 = {
-          type                     = "ingress"
-          from_port                = 80
-          to_port                  = 80
-          protocol                 = "tcp"
-          description              = "Service port"
-          source_security_group_id = aws_security_group.allow_http.id
-        }
-        egress_all = {
-          type        = "egress"
-          from_port   = 0
-          to_port     = 0
-          protocol    = "-1"
-          cidr_blocks = ["0.0.0.0/0"]
-        }
-      }
-    }
+  network_configuration {
+    subnets         = local.private_subnet_ids
+    security_groups = [aws_security_group.ecs.id]
   }
 }
--- terraform/backend/sg.tf
+++ terraform/backend/sg.tf
@@ -1,6 +1,6 @@
-resource "aws_security_group" "allow_http" {
-  name        = "allow_http"
-  description = "Allow http inbound traffic"
+resource "aws_security_group" "lb" {
+  name        = "alb-security-group"
+  description = "Allow http inbound traffic on the alb"
   vpc_id      = local.vpc_id
 
   ingress {
@@ -17,3 +17,51 @@ resource "aws_security_group" "allow_http" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "ecs" {
+  name        = "ecs-security-group"
+  description = "Allow inbound traffic to the ecs task"
+  vpc_id      = local.vpc_id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name        = "rds-security-group"
+  description = "Allow Postgres traffic from private ranges"
+  vpc_id      = local.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
